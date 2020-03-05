d83e8623229f95a98bf9c4ba640406ef261cf862
Urs Utzinger <uutzinger@gmail.com>
2020-03-05 11:26:52 -0700

    Update test_networktable_plot.py

--- test_networktable_plot.py
+++ test_networktable_plot.py
@@ -19,18 +19,38 @@ print("Display Capture")
 
 print("Getting Dat")
 
+# Network Table attach
+NetworkTables.initialize(server="10.41.83.2")
+NetworkTables.addConnectionListener(connectionListener, immediateNotify=True)
+
+sd = NetworkTables.getTable("SmartDashboard")
+ll = NetworkTables.getTable("limelight")
+
+
+
 last_fps_time = time.time()
 while(True):
 
+    robotTime = sd.getNumber("dsTime", -1))
+
     # Plot Cross Hair
+    # at center of graph
     
     # Plot Target Center
+    tv=llt.getNumber('tv')
+    tx=ll.getNumber('tx')
+    ty=ll.getNumber('ty')
+    ax.plot(tx, ty, {'marker': 'o'})
     
     # Plot Corners`
+    corner_x=ll.getNumber('tcornx')
+    corner_y=ll.getNumber('tcorny')
+    
     ax.plot(corners_x, corners_y, {'marker': 'o'})
+    
     ax.grid()
     ax.set_xlim([0,320])
     ax.set_ylim([0,240])
     
-
     plt.show()
+
