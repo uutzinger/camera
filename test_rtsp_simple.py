5f9c1e1322a3d7dd46f085f484c9c797737f0289
Urs Utzinger <uutzinger@gmail.com>
2020-03-08 16:34:14 -0700

    Create test_rtsp_simple.py

---
+++ test_rtsp_simple.py
@@ -0,0 +1,30 @@
+import cv2
+import time
+
+rtsp="rtsp://10.41.83.100:554/camera"
+
+print("Starting Capture")
+cap = cv2.VideoCapture(rtsp, apiPreference=cv2.CAP_GSTREAMER)
+
+print("Getting Frames")
+num_frames = 0
+window_handle = cv2.namedWindow("Camera", cv2.WINDOW_NORMAL)
+last_fps_time = time.time()
+
+while(cv2.getWindowProperty("Camera", 0) >= 0):
+    current_time = time.time()
+    ret, frame = cap.read()
+    if not frame == None:
+        num_frames += 1
+        cv2.imshow('RTSP', frame)
+    if cv2.waitKey(20) & 0xFF == ord('q'):
+        break
+    if (current_time - last_fps_time) >= 5.0: # update frame rate every 5 secs
+        measured_fps = num_frames/5.0
+        print( "Status:FPS:{}".format(measured_fps))
+        num_frames = 0
+        last_fps_time = current_time
+cap.release()
+cv2.destroyAllWindows()
+
+# gst-launch-1.0 playbin uri=rtsp://10.41.83.100:554/camera
