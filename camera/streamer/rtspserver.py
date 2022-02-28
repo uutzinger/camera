e206ed157a2548311081549877470b0824d70261
Urs Utzinger <utzinger@email.arizona.edu>
2022-02-28 14:54:07 -0700

    Update rtspserver.py

--- camera/streamer/rtspserver.py
+++ camera/streamer/rtspserver.py
@@ -1,6 +1,5 @@
- 
 ###############################################################################
-# RTsP server
+# RTSP server
 # 2022 Initial Release
 # Urs Utzinger 
 ###############################################################################
@@ -9,6 +8,23 @@
 # https://github.com/tamaggo/gstreamer-examples/blob/master/test_gst_rtsp_server.py
 # https://github.com/BreeeZe/rpos/blob/master/python/gst-rtsp-launch.py
 # https://jonic.cn/?qa=560930/
+
+
+TypeError: do_create_element() takes 1 positional argument but 2 were given
+
+** (python3:1867): CRITICAL **: 08:45:46.493: could not create element
+^CTraceback (most recent call last):
+  File "rtspserver.py", line 148, in <module>
+    rtsp.start()
+  File "rtspserver.py", line 126, in start
+    loop.run()
+  File "/usr/lib/python3/dist-packages/gi/overrides/GLib.py", line 498, in run
+    super(MainLoop, self).run()
+  File "/usr/lib/python3.7/contextlib.py", line 119, in __exit__
+    next(self.gen)
+  File "/usr/lib/python3/dist-packages/gi/_ossighelper.py", line 251, in register_sigint_fallback
+    signal.default_int_handler(signal.SIGINT, None)
+
 
 ###############################################################################
 # Imports
@@ -25,7 +41,7 @@ import time
 import gi
 gi.require_version('Gst', '1.0')
 gi.require_version('GstRtspServer', '1.0')
-from gi.repository import Gst, GstRtspServer, GObject
+from gi.repository import Gst, GstRtspServer, GObject, GLib
 
 ###############################################################################
 # RTSP Server
@@ -55,12 +71,12 @@ class rtspFactory(GstRtspServer.RTSPMediaFactory):
             #if retval != Gst.FlowReturn.OK:
             #    print(retval)
 
-    def do_create_element(self):
+    def do_create_element(self, uri):
         s_src  = 'appsrc name=source is-live=true block=true format=GST_FORMAT_TIME ' \
                  'caps=video/x-raw,format=BGR,width={},height={},framerate={}/1 '.format(self._res[0], self.__res[1], self._fps)
 
         s_h264 = '! videoconvert ! video/x-raw,format=I420 ' \
-                 '! x264enc speed-preset=ultrafast tune=zerolatency bitrate={} threads=0 '.format(self._bitrate/1000)
+                 '! x264enc speed-preset=ultrafast tune=zerolatency bitrate={} threads=4 '.format(self._bitrate/1000)
         
         #s_h264 = "x264enc speed-preset=ultrafast tune=zerolatency threads=4 "
         #s_src = "v4l2src ! video/x-raw,rate=30,width=320,height=240 ! videoconvert ! video/x-raw,format=I420"
@@ -68,7 +84,19 @@ class rtspFactory(GstRtspServer.RTSPMediaFactory):
 		#s_src  = "videotestsrc ! video/x-raw,rate=30,width=320,height=240,format=I420 "
         #s_h264 = "omx264enc control-rate=1 target-bitrate={} ".format(self._bitrate)
   
-        pipeline_str = "( {} ! queue max-size-buffers=1 name=q_enc ! {} ! rtph264pay config-interval=1 name=pay0 pt=96 )".format(s_src, s_h264)
+        # Make sure simple test pipelines work
+        # ./test-launch "( videotestsrc ! x264enc ! rtph264pay name=pay0 pt=96 )"
+        # ./test-launch "( videotestsrc ! x264enc speed-preset=ultrafast ! rtph264pay name=pay0 pt=96 )"
+        # ./test-launch "( videotestsrc ! x264enc speed-preset=ultrafast ! rtph264pay name=pay0 pt=96 )"
+        # ./test-launch "( videotestsrc ! x264enc speed-preset=ultrafast tune=zerolatency ! rtph264pay name=pay0 pt=96 )"
+        # ./test-launch "( videotestsrc ! x264enc speed-preset=ultrafast tune=zerolatency threads=4 ! rtph264pay name=pay0 pt=96 )"
+        # ./test-launch "( videotestsrc ! x264enc speed-preset=ultrafast tune=zerolatency bitrate=1000000 threads=4 ! rtph264pay name=pay0 pt=96 )"
+        # ./test-launch "( videotestsrc ! video/x-raw,rate=30,width=640,height=480,format=I420 ! x264enc speed-preset=ultrafast tune=zerolatency bitrate=1000000 threads=4 ! rtph264pay config-interval=1 name=pay0 pt=96 )"
+        # ./test-launch "( v4l2src device=/dev/video0 ! video/x-h264, width=1280, height=720, framerate=15/1 ! h264parse config-interval=1 ! rtph264pay name=pay0 pt=96 )"
+        # ./test-launch "( v4l2src device=/dev/video0 ! video/x-h264, width=1280, height=720, framerate=15/1 ! queue max-size-buffers=1 name=q_enc ! h264parse config-interval=1 ! rtph264pay name=pay0 pt=96 )"
+
+        pipeline_str = "( {}! queue max-size-buffers=1 name=q_enc {}! rtph264pay config-interval=1 name=pay0 pt=96 )".format(s_src, s_h264)
+
         return Gst.parse_launch(pipeline_str)
 
     def do_configure(self, rtsp_media):
@@ -79,10 +107,10 @@ class rtspFactory(GstRtspServer.RTSPMediaFactory):
 class rtspServer(GstRtspServer.RTSPServer):
             
     def __init__(self, 
-        resolution: tuple(int, int) = (320, 240), 
-        fps: int                    = 15,
-        bitrate: int                = 2048,
-        stream_uri : str            = '/test'):
+        resolution: tuple = (320, 240), 
+        fps: int          = 15,
+        bitrate: int      = 2048,
+        stream_uri : str  = '/test'):
 
         # Threading Queue, Locks, Events
         self.queue           = Queue(maxsize=32)
@@ -100,7 +128,7 @@ class rtspServer(GstRtspServer.RTSPServer):
         self.factory    = rtspFactory()
         self.factory.set_shared(True)
         self.mountpoint = self.server.get_mount_points()
-        self.mountpint.add_factory(self._uri, self.factory)
+        self.mountpoint.add_factory(self._uri, self.factory)
         self.server.attach(None)
         self.stopped = False
     
@@ -108,9 +136,9 @@ class rtspServer(GstRtspServer.RTSPServer):
         self.stopped = False
         
     def start(self):
-        GObject.threads_init()
+        # GObject.threads_init()
         Gst.init(None)
-
+        
         loop = GLib.MainLoop()
         loop.run()
    
@@ -120,8 +148,8 @@ if __name__ == '__main__':
 
     fps=  30
     display_interval = 1./fps
-    height =540
-    width = 720
+    height =480
+    width = 640
     depth = 3
 
     logging.basicConfig(level=logging.DEBUG)
