e2b032a5020ff727be5772ec55e4f5a0b5430920
Urs Utzinger <utzinger@email.arizona.edu>
2022-03-02 22:07:18 -0700

    Update rtspserver.py

--- camera/streamer/rtspserver.py
+++ camera/streamer/rtspserver.py
@@ -4,28 +4,18 @@
 # Urs Utzinger 
 ###############################################################################
 
+# Implementing GstRtspServer
 # https://github.com/mad4ms/python-opencv-gstreamer-examples/blob/master/gst_rtsp_server.py#L43
 # https://github.com/tamaggo/gstreamer-examples/blob/master/test_gst_rtsp_server.py
 # https://github.com/BreeeZe/rpos/blob/master/python/gst-rtsp-launch.py
 # https://jonic.cn/?qa=560930/
+#
+# Implement the response to this question
+# https://stackoverflow.com/questions/47396372/write-opencv-frames-into-gstreamer-rtsp-server-pipeline
 
-
-TypeError: do_create_element() takes 1 positional argument but 2 were given
-
-** (python3:1867): CRITICAL **: 08:45:46.493: could not create element
-^CTraceback (most recent call last):
-  File "rtspserver.py", line 148, in <module>
-    rtsp.start()
-  File "rtspserver.py", line 126, in start
-    loop.run()
-  File "/usr/lib/python3/dist-packages/gi/overrides/GLib.py", line 498, in run
-    super(MainLoop, self).run()
-  File "/usr/lib/python3.7/contextlib.py", line 119, in __exit__
-    next(self.gen)
-  File "/usr/lib/python3/dist-packages/gi/_ossighelper.py", line 251, in register_sigint_fallback
-    signal.default_int_handler(signal.SIGINT, None)
-
-
+# Dealing with child and parent class, why we need super() 
+# https://stackoverflow.com/questions/55032685/child-class-inherit-parent-class-self-variable
+  
 ###############################################################################
 # Imports
 ###############################################################################
@@ -49,35 +39,19 @@ from gi.repository import Gst, GstRtspServer, GObject, GLib
 
 class rtspFactory(GstRtspServer.RTSPMediaFactory):
 
-    def __init__(self):
-        GstRtspServer.RTSPMediaFactory.__init__(self)
-
-    def on_need_data(self, src, length):        
-        if not self.stopped:
-            (frame_time, frame) = self.queue.get(block=True, timeout=None)            
-            data = frame.tobytes()
-            buf = Gst.Buffer.new_allocate(None, len(data), None)
-            buf.fill(0, data)
-            buf.duration = self._duration
-            timestamp = self.number_frames * self._duration
-            buf.pts = buf.dts = int(timestamp)
-            buf.offset = timestamp
-            self._number_frames += 1
-            retval = src.emit('push-buffer', buf)
-            # should add belwo to debug logger
-            # print('pushed buffer, frame {}, duration {} ns, durations {} s'.format(self.number_frames,
-            #                                                                        self.duration,
-            #                                                                        self.duration / Gst.SECOND))
-            #if retval != Gst.FlowReturn.OK:
-            #    print(retval)
-
-    def do_create_element(self, uri):
+    def __init__(self, **args):
+        super(rtspFactory, self).__init__(**args) # this should allow rtspFactory inherit self
+        self._number_frames = 0
+        self._duration = 1. / self._fps * Gst.SECOND
+        # 
         s_src  = 'appsrc name=source is-live=true block=true format=GST_FORMAT_TIME ' \
                  'caps=video/x-raw,format=BGR,width={},height={},framerate={}/1 '.format(self._res[0], self.__res[1], self._fps)
-
+        # could remove all options provided to appsrc
+        
         s_h264 = '! videoconvert ! video/x-raw,format=I420 ' \
                  '! x264enc speed-preset=ultrafast tune=zerolatency bitrate={} threads=4 '.format(self._bitrate/1000)
-        
+        # could remove ! video/x-raw,format=I420
+                
         #s_h264 = "x264enc speed-preset=ultrafast tune=zerolatency threads=4 "
         #s_src = "v4l2src ! video/x-raw,rate=30,width=320,height=240 ! videoconvert ! video/x-raw,format=I420"
         #s_h264 = "videoconvert ! vaapiencode_h264 bitrate=1000"
@@ -85,19 +59,44 @@ class rtspFactory(GstRtspServer.RTSPMediaFactory):
         #s_h264 = "omx264enc control-rate=1 target-bitrate={} ".format(self._bitrate)
   
         # Make sure simple test pipelines work
+        # check the audio version and add...
         # ./test-launch "( videotestsrc ! x264enc ! rtph264pay name=pay0 pt=96 )"
         # ./test-launch "( videotestsrc ! x264enc speed-preset=ultrafast ! rtph264pay name=pay0 pt=96 )"
         # ./test-launch "( videotestsrc ! x264enc speed-preset=ultrafast ! rtph264pay name=pay0 pt=96 )"
         # ./test-launch "( videotestsrc ! x264enc speed-preset=ultrafast tune=zerolatency ! rtph264pay name=pay0 pt=96 )"
         # ./test-launch "( videotestsrc ! x264enc speed-preset=ultrafast tune=zerolatency threads=4 ! rtph264pay name=pay0 pt=96 )"
         # ./test-launch "( videotestsrc ! x264enc speed-preset=ultrafast tune=zerolatency bitrate=1000000 threads=4 ! rtph264pay name=pay0 pt=96 )"
-        # ./test-launch "( videotestsrc ! video/x-raw,rate=30,width=640,height=480,format=I420 ! x264enc speed-preset=ultrafast tune=zerolatency bitrate=1000000 threads=4 ! rtph264pay config-interval=1 name=pay0 pt=96 )"
+        # width audio
+        # ./test-launch "( videotestsrc ! video/x-raw,width=352,height=288,framerate=15/1,format=I420 ! x264enc speed-preset=ultrafast tune=zerolatency bitrate=1000000 threads=4 ! rtph264pay config-interval=1 name=pay0 pt=96 audiotestsrc ! audio/x-raw,rate=8000 ! alawenc ! rtppcmapay name=pay1 pt=8 )"
+        # with real video
         # ./test-launch "( v4l2src device=/dev/video0 ! video/x-h264, width=1280, height=720, framerate=15/1 ! h264parse config-interval=1 ! rtph264pay name=pay0 pt=96 )"
         # ./test-launch "( v4l2src device=/dev/video0 ! video/x-h264, width=1280, height=720, framerate=15/1 ! queue max-size-buffers=1 name=q_enc ! h264parse config-interval=1 ! rtph264pay name=pay0 pt=96 )"
 
-        pipeline_str = "( {}! queue max-size-buffers=1 name=q_enc {}! rtph264pay config-interval=1 name=pay0 pt=96 )".format(s_src, s_h264)
+        self.pipeline_str = "( {}! queue max-size-buffers=1 name=q_enc {}! rtph264pay config-interval=1 name=pay0 pt=96 )".format(s_src, s_h264)
+        # coult remove queue max ... but need rtph264pay0
+        if not self.log.full(): self.log.put_nowait((logging.DEBUG, "create stream {}".format(self.pipeline_str)))
+        
+    def on_need_data(self, src, length):        
+        if not self.stopped:
+            (frame_time, frame) = self.queue.get(block=True, timeout=None)            
+            data = frame.tobytes()
+            buf = Gst.Buffer.new_allocate(None, len(data), None)
+            buf.fill(0, data)
+            buf.duration = self._duration
+            timestamp = self._number_frames * self._duration
+            buf.pts = buf.dts = int(timestamp)
+            buf.offset = timestamp
+            self._number_frames += 1
+            retval = src.emit('push-buffer', buf)
+            if retval != Gst.FlowReturn.OK:0
+                if not self.log.full(): self.log.put_nowait(( logging.DEBUG, "pushed buffer error {}".format(retval) ))
+            if not self.log.full(): self.log.put_nowait((logging.DEBUG, "pushed buffer,frame{},duration{}ns,durations{}s".format(self.number_frames,self.duration,self.duration / Gst.SECOND)))
 
-        return Gst.parse_launch(pipeline_str)
+    def do_create_element(self, url):
+        return Gst.parse_launch(self.pipeline_str)
+        # self.appsrc       = self.pipeline.get_child_by_index(4)
+        # GstRtspServer.RTSPMediaFactory.__init__(self)
+
 
     def do_configure(self, rtsp_media):
         self._number_frames = 0
@@ -110,7 +109,9 @@ class rtspServer(GstRtspServer.RTSPServer):
         resolution: tuple = (320, 240), 
         fps: int          = 15,
         bitrate: int      = 2048,
-        stream_uri : str  = '/test'):
+        stream_uri : str  = '/test', **args):
+        
+        super(rtspServer, self).__init__(**args) # not sure what this is for as we dont need to inherit from main
 
         # Threading Queue, Locks, Events
         self.queue           = Queue(maxsize=32)
@@ -124,23 +125,24 @@ class rtspServer(GstRtspServer.RTSPServer):
         self._uri     = stream_uri    
         self._duration = 1. / self._fps * Gst.SECOND  # duration of a frame in nanoseconds
         
-        self.server     = GstRtspServer.RTSPServer()
+        # self.server     = GstRtspServer.RTSPServer()
         self.factory    = rtspFactory()
         self.factory.set_shared(True)
-        self.mountpoint = self.server.get_mount_points()
-        self.mountpoint.add_factory(self._uri, self.factory)
-        self.server.attach(None)
+        self.mountpoints = self.server.get_mount_points()
+        self.mountpoints.add_factory(self._uri, self.factory)
+        self.attach(None)
+        # self.server.attach(None)
         self.stopped = False
     
     def stop(self):
-        self.stopped = False
+        self.stopped = True
         
     def start(self):
-        # GObject.threads_init()
+        # GObject.threads_init(), depreicated?
         Gst.init(None)
         
-        loop = GLib.MainLoop()
-        loop.run()
+        self.loop = GLib.MainLoop()
+        self.loop.run()
    
 if __name__ == '__main__':
 
